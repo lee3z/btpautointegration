542e2258919ff4bfe09907448d40acf83ee5c7d8
eunseong.lee <stopmusic3z@gmail.com>
2024-09-08 13:52:55 +0000

    initial commit

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["com/sap/zfioritraining/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"comsap/zfiori_training/test/unit/controller/Home.controller"
+], function () {
+	"use strict";
+});
